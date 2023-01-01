157a75a4a52a70b6a32c3fc446a1216f88d409a0
Abhishek Pawar <77561736+abhiishekpawar@users.noreply.github.com>
2023-01-01 21:08:05 +0530

    Update HelloWorld.java

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -1,25 +1,33 @@
-package intrrr;
-import java.util.Scanner;
+import java.util.*;
 
-public class Count_vowel_cons {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int vowels =0;
-        int consonant = 0;
-        char[] ch = s.toCharArray();
-
-        for(int i=0;i<ch.length;i++)
-        {
-            if(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u')
-            {
-                vowels+=1;
-            }
-            else {
-                consonant += 1;
-            }
-        }
-
-        System.out.println("Number of vowels are "+vowels+" Number of consonant are "+consonant);
-    }
+public class Mock {
+	int b = 20;
+	protected int c = 30;
+	public int d = 40;
+	private int a  =10;
+	
+	public void method() {
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+	
+	public static void main(String[] args) {
+		Mock m = new Mock();
+		System.out.println(m.a);
+		System.out.println(m.b);
+		System.out.println(m.c);
+		System.out.println(m.d);
+		
+	}
 }
+
+
+/*
+Access Modifier 	within class	within package	outside package by subclass only	outside package
+
+Private             	Y	                N	            N	                                N
+Default	                Y	                Y           	N                               	N
+Protected	            Y       	        Y           	Y                               	N
+Public	                Y	                Y           	Y	                                Y
