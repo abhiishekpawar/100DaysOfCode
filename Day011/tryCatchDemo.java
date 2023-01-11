d9791d63094e9cacd66e10af6000e6c882870739
Abhishek Pawar <77561736+abhiishekpawar@users.noreply.github.com>
2023-01-11 20:39:56 +0530

    Create tryCatchDemo.java

---
+++ Day011/tryCatchDemo.java
@@ -0,0 +1,11 @@
+public class Mock {
+	public static void main(String[] args) {
+		
+		try {
+			int i=10/0;
+			System.out.println(i);
+		}catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+	}
+}
