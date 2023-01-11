bac11ec75bef74899ef8611feee7c7c0e5e90da3
Abhishek Pawar <77561736+abhiishekpawar@users.noreply.github.com>
2023-01-11 20:40:54 +0530

    Update tryCatchDemo.java

--- Day011/tryCatchDemo.java
+++ Day011/tryCatchDemo.java
@@ -9,3 +9,18 @@ public class Mock {
 		}
 	}
 }
+
+public class Mock {
+	public static void main(String[] args) {
+		
+		try {
+			int i=10/0;
+			System.out.println(i);
+		}catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
